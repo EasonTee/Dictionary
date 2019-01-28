e798f25b7821d5783f067815cd23425e3ddad8a7
EasonTee <37705010+EasonTee@users.noreply.github.com>
2019-01-28 23:12:04 +0800

    Update __init__.py
    Add a secret key for Flask WT form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '\t\xa9\x7f\x86mA#\xa0\x8a\x81I\x7f<\x7f;\x11\xe2v*\xc1}0T\x92'
 
 from app import routes
